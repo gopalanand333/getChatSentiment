b128244e1a82b5315d396c960b0b2a8b7b5d7e70
Gopal Anand <32189444+gopalanand333@users.noreply.github.com>
2019-04-25 10:56:59 +0530

    working with realtime chart when there is new sentiment data analyzed

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 /*jshint esversion: 8 */
 const express = require("express");
 const fetch = require("node-fetch");
+
 // Imports the Google Cloud client library
 const language = require('@google-cloud/language');
 const request = require("request");
 // Instantiates a client
 const client = new language.LanguageServiceClient();
 const app = express();
+//const server = require("http").Server(app);
+
 var streamData = [];
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 8088;
+const server = app.listen(port);
+const io = require("socket.io").listen(server);
+
+
+
 app.use(bodyParser.json()); // support encoded json bodies
 app.use(bodyParser.urlencoded({
     extended: true
 })); // supports encoded bodies
 //use html page
-app.use(express.static(__dirname+"/charts"));
+app.use(express.static(__dirname + "/charts"));
 // Post port http://localhost:8090/analyzeSentiment
 app.post("/analyzeSentiment", (req, res) => {
     var payload = req.body.message;
@@ -27,10 +35,18 @@ app.get("/getData", (req, res) => {
     console.log(streamData);
     res.send(streamData);
 });
-app.listen(port, () => {
-    console.log("App is listening to port :-" + port);
-});
+// server.listen(port, () => {
+//     console.log("App is listening to port :-" + port);
+// });
 
+
+io.on("connection", function(socket){
+    console.log("connection established");
+    socket.on("disconnect",function(){
+        console.log("disconnected");
+    });
+  
+});
 // function to call google apis for sentiment analysis
 function analyzeSentimentData(textStream) {
     var data = {
@@ -41,7 +57,7 @@ function analyzeSentimentData(textStream) {
         "encodingType": "UTF8"
     };
     var that = this;
-    
+
     console.log(JSON.stringify(data));
     request.post("https://language.googleapis.com/v1/documents:analyzeSentiment?fields=documentSentiment&key={apiKey}", {
         json: data
@@ -50,9 +66,7 @@ function analyzeSentimentData(textStream) {
             console.log(err);
         }
         console.log(body.documentSentiment.score);
-        //pushes data every time there's a message recieved 
-        console.log(body.documentSentiment);
-        console.log(streamData);
-    streamData.push(body.documentSentiment.score);
+        // socket implementation
+        io.emit("dataStream",body.documentSentiment);
     });
 }
